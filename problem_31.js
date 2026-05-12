a48b4d723229e40bc56d84115b0d9f4c5f448d3b
ekrambd <ekramhossainekram28@gmail.com>
2026-05-12 23:20:18 +0600

    problem 31 solved

---
+++ problem_31.js
@@ -0,0 +1,17 @@
+//31. Reverse Words
+
+var reverseWords = function(str){
+    let arr = str.split(" ");
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right){
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+    return arr.join(" ");
+};
+
+console.log(reverseWords("I Love Bangladesh"));
