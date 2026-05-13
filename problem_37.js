7649e65ea4eed43d758726407bdc0198526e40bc
ekrmabd <ekramhossainekram28@gmail.com>
2026-05-13 18:20:04 +0600

    problem 37 solved

---
+++ problem_37.js
@@ -0,0 +1,17 @@
+//37. Reverse String
+
+var reverseString = function(str){
+    let arr = str.split("");
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right){
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+    return arr.join('');
+};
+
+console.log(reverseString("hello"));
